d348cc464310774a0f17de10ca7f9c70adac0173
sejalabhangrao <sejalabhangrao@gmail.com>
2016-12-22 14:14:08 +0530

    Added users details edit option

--- app/src/main/java/com/l3cube/catchup/ui/activities/EditInfoActivity.java
+++ app/src/main/java/com/l3cube/catchup/ui/activities/EditInfoActivity.java
@@ -1,11 +1,14 @@
 package com.l3cube.catchup.ui.activities;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -40,11 +43,15 @@ public class EditInfoActivity extends AppCompatActivity {
         TextView lname = (TextView) findViewById(R.id.lastNameLabel);
         TextView email = (TextView) findViewById(R.id.emailAddressLabel);
         TextView birthDate = (TextView) findViewById(R.id.birthDateLabel);
+        TextView mPhoneNumber = (TextView) findViewById(R.id.contactNumberLabel);
+        Button mEditDetails = (Button) findViewById(R.id.editDetailsButton);
         //ImageView mImageView = findViewById(R.id.profilePictureLabel);
+        fname.setText(ParseUser.getCurrentUser().getString("firstName"));
         lname.setText(ParseUser.getCurrentUser().getString("lastName"));
         email.setText(ParseUser.getCurrentUser().getString("emailId"));
         birthDate.setText(ParseUser.getCurrentUser().getString("birthDate"));
-        fname.setText(ParseUser.getCurrentUser().getString("firstName"));
+        mPhoneNumber.setText(ParseUser.getCurrentUser().getString("mobileNumber"));
+
 
         //ImageView mprofilePictureView;
         //mprofilePictureView = (ImageView) findViewById(R.id.profilePictureLabel);
@@ -54,6 +61,14 @@ public class EditInfoActivity extends AppCompatActivity {
 
         new DownloadImageTask((ImageView) findViewById(R.id.profilePictureLabel))
                 .execute(mImageUrl);
+
+
+        mEditDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EditInfoActivity.this, EditUserDetailsActivity.class));
+            }
+        });
 
         //Log.d(TAG, "This is image Url" + mImageUrl);
 
