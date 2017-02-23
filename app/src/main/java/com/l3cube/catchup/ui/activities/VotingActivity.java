f4891289855e03b6e7c8674930c150fbc4675016
sejalabhangrao <sejalabhangrao@gmail.com>
2017-02-24 00:18:49 +0530

    Poll choices vertically one below the other

--- app/src/main/java/com/l3cube/catchup/ui/activities/VotingActivity.java
+++ app/src/main/java/com/l3cube/catchup/ui/activities/VotingActivity.java
@@ -1,9 +1,15 @@
 package com.l3cube.catchup.ui.activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -16,10 +22,15 @@ import com.l3cube.catchup.R;
 
 public class VotingActivity extends AppCompatActivity {
 
-    private ImageView addChoice;
-    private RelativeLayout mLayout;
+   // private ImageView addChoice;
+    //private RelativeLayout mLayout;
+    //private EditText mEditText;
+   // private Button mAddChoice;
+    //private int count;
+
+    private LinearLayout mLayout;
     private EditText mEditText;
-    private int count;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,41 +43,64 @@ public class VotingActivity extends AppCompatActivity {
         int height = dm.heightPixels;
         getWindow().setLayout((int)(width*0.8),(int)(height*0.6));
 
-        addChoice = (ImageView) findViewById(R.id.btn_add_choice);
+        /*addChoice = (ImageView) findViewById(R.id.btn_add_choice);
         mLayout = (RelativeLayout) findViewById(R.id.activity_voting);
         mEditText = (EditText) findViewById(R.id.choice1);
         TextView textView = new TextView(this);
         textView.setText("Choice");
         setupListeners();
 
+        addChoice.setOnClickListener(onClick());
+        //addChoice.setOnClickListener();*/
+
+
+        mLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(onClick());
+        TextView textView = new TextView(this);
+        textView.setText("New text");
 
     }
-    private void setupListeners(){
 
-        addChoice.setOnClickListener(new View.OnClickListener() {
+    private View.OnClickListener onClick() {
+        return new View.OnClickListener() {
 
             @Override
-            public void onClick(View view) {
-
-
-               // mEditText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                mLayout.addView(mEditText);
-                //Toast.makeText(VotingActivity.this,count,Toast.LENGTH_SHORT).show();
-
-
-
+            public void onClick(View v) {
+                mLayout.addView(createNewTextView(mEditText.getText().toString()));
             }
-        });
+        };
     }
 
 
+    private TextView createNewTextView(String text) {
+        final ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        //final TextView textView = new TextView(this);
+        final TextView textView = (TextView) new EditText(this);
+        textView.setLayoutParams(lparams);
+        //textView.setText("New text: " + text);
+        textView.setHint("Add Choice " + text);
+        return textView;
+    }
 
-private TextView createNewTextView(String text){
-    final RelativeLayout.LayoutParams lparams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-    final TextView textView = new TextView(this);
-    textView.setLayoutParams(lparams);
-    textView.setText(text);
-    return textView;
-}
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.edit_catchup_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Take appropriate action for each action item click
+        switch (item.getItemId()) {
+            case R.id.action_bar:
+               //EditDetails();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
